d031e063d124d5bfeaaf5b08adfc86b45f087ada
Saurabey ?? <saurabbaral03@gmail.com>
2023-12-29 09:38:46 +0545

    completed_river_solving_problem

--- river_problem.py
+++ river_problem.py
@@ -1,76 +1,90 @@
 def is_valid(state):
     farmer, goat, wolf, cabbage = state
 
-    if (goat == wolf and farmer != goat) and (goat == cabbage and farmer != goat):
+    if (goat == wolf and farmer != goat) or (goat == cabbage and farmer != goat):
         return False
     return True
 
 
 
-def tree(start_state, visited, tree_structure):
+def tree(start_state):
+
+
+    second_height, count, index1 = nodes_at_height(start_state, 0)
+    third_height, count, index2 = nodes_at_height(second_height, count)
+    fourth_height, count, index3 = nodes_at_height(third_height, count)
+    fifth_height, count, index4 = nodes_at_height(fourth_height, count)
+    sixth_tree_structure, count, index5 = nodes_at_height(fifth_height, count)
+    seventh_tree_structure, count, index6 = nodes_at_height(sixth_tree_structure, count)
+    eighth_tree_structure, count, index7 = nodes_at_height(seventh_tree_structure, count)
+    # total_tree = second_height+third_height+fourth_height+fifth_height+sixth_tree_structure+seventh_tree_structure+eighth_tree_structure
+    total_tree = [[start_state],second_height, third_height, fourth_height, fifth_height, sixth_tree_structure, seventh_tree_structure, eighth_tree_structure]
+    index = [[0],index1, index2, index3, index4, index5, index6, index7]
     
-
-    second_height, count = nodes_at_height((0,0,0,0), 0)
-    
-    third_height, count = nodes_at_height(second_height, count)
-    fourth_height, count = nodes_at_height(third_height, count)
-    fifth_height, count = nodes_at_height(fourth_height, count)
-    sixth_tree_structure, count = nodes_at_height(fifth_height, count)
-    seventh_tree_structure, count = nodes_at_height(sixth_tree_structure, count)
-    eighth_tree_structure, count = nodes_at_height(seventh_tree_structure, count)
-
-    print(len(second_height))
-    print(len(third_height))
-    print(len(fourth_height))
-    print(len(fifth_height))
-    print(len(sixth_tree_structure))
-    print(len(seventh_tree_structure))
-    print(len(eighth_tree_structure))
-
-
-    
+    return total_tree, index
 
 
 
 def nodes_at_height(previous_height_nodes, count):
-    print(count)
-    print("*************************************")
     if isinstance(previous_height_nodes, tuple):
         previous_height_nodes = [previous_height_nodes]
-
+    index = []
     west_east = count % 2
     next_height_nodes = []
-    for i in previous_height_nodes:
+    for h,i in enumerate(previous_height_nodes):
         new_objects = list(i)
-        print('i', i)
+
         for j,k in enumerate(new_objects):
             if j == 0:
+                index.append(h)
                 new_objects[j] = abs(west_east - 1)
                 next_height_nodes.append(tuple(new_objects))
-                print("object", new_objects)
 
             elif new_objects[j] == abs(west_east):
+                index.append(h)
                 new_objects[j] = abs(west_east - 1)
                 next_height_nodes.append(tuple(new_objects))
-                print("object", new_objects)
                 new_objects[j] = abs(west_east)
     count += 1
-    return next_height_nodes, count
+    # print("index", index)
+    return next_height_nodes, count, index
 
-    
+def dfs(total_tree, visited, index):
+    stack = [(0, 0)]
+    steps = []
+    while stack:
+        height, index_value = stack.pop()
+        node = total_tree[height][index_value]
+        if node not in visited and is_valid(node):
+            print("Step:", node)
+            steps.append(node)
+            visited.add(node)
 
-# def dfs(tree_structure, visited):
-                        
-# def dfs(tree_structure, visited):
+            if height + 1 < len(total_tree):
+                for i, child_node in enumerate(total_tree[height + 1]):
+                    if index[height + 1][i] == index_value:  
+                        stack.append((height + 1, i))
+    return steps
+
+
+def create_story(steps):
+    dict1 = {0: "left", 1: "right"}
+    for state in steps:
+        farmer, goat, wolf, cabbage = state
+        print(f"\nfarmer: {dict1[farmer]}\t goat: {dict1[goat]}\t wolf: {dict1[wolf]}\t cabbage: {dict1[cabbage]}")
+
+
 
 
 
 def main():
+    print("\nPUR077BCT075 == Saurab Baral\n")
     start_state = (0,0,0,0)
     visited = set()
     tree_structure = []
-    new_tree = tree(start_state,visited, tree_structure )
-    # dfs(tree_structure, visited)
-    print(tree_structure)
+    total_tree, index = tree(start_state)
+    steps = dfs(total_tree, visited, index)
+    create_story(steps)
+
 
 main()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,81 +1,82 @@
-from collections import deque
-
-# Function to check if the state is valid
 def is_valid(state):
     farmer, goat, wolf, cabbage = state
-    # Check for invalid states based on the rules
-    if (wolf == goat and farmer != wolf) or (goat == cabbage and farmer != goat):
+
+    if (goat == wolf and farmer != goat) or (goat == cabbage and farmer != goat):
         return False
     return True
 
-# Function to generate possible moves
-def generate_moves(state):
-    moves = []
-    farmer, goat, wolf, cabbage = state
-    # Possible movements of the farmer
-    possible_farmer_moves = [0, 1]
-    for move in possible_farmer_moves:
-        new_farmer = farmer ^ move
-        for passenger in range(4):
-            # Exclude illegal states
-            if passenger != farmer:
-                new_state = (new_farmer, goat, wolf, cabbage)
-                new_state = list(new_state)
-                new_state[passenger] = 1 - new_state[passenger]
-                new_state = tuple(new_state)
-                moves.append(new_state)
-    return moves
 
-# Breadth-First Search algorithm
-def breadth_first_search():
-    start_state = (0, 0, 0, 0)
+
+def tree(start_state):
+
+
+    second_height, count, index1 = nodes_at_height(start_state, 0)
+    third_height, count, index2 = nodes_at_height(second_height, count)
+    fourth_height, count, index3 = nodes_at_height(third_height, count)
+    fifth_height, count, index4 = nodes_at_height(fourth_height, count)
+    sixth_tree_structure, count, index5 = nodes_at_height(fifth_height, count)
+    seventh_tree_structure, count, index6 = nodes_at_height(sixth_tree_structure, count)
+    eighth_tree_structure, count, index7 = nodes_at_height(seventh_tree_structure, count)
+    # total_tree = second_height+third_height+fourth_height+fifth_height+sixth_tree_structure+seventh_tree_structure+eighth_tree_structure
+    total_tree = [[start_state],second_height, third_height, fourth_height, fifth_height, sixth_tree_structure, seventh_tree_structure, eighth_tree_structure]
+    index = [[0],index1, index2, index3, index4, index5, index6, index7]
+    
+    return total_tree, index
+
+
+
+def nodes_at_height(previous_height_nodes, count):
+    if isinstance(previous_height_nodes, tuple):
+        previous_height_nodes = [previous_height_nodes]
+    index = []
+    west_east = count % 2
+    next_height_nodes = []
+    for h,i in enumerate(previous_height_nodes):
+        new_objects = list(i)
+
+        for j,k in enumerate(new_objects):
+            if j == 0:
+                index.append(h)
+                new_objects[j] = abs(west_east - 1)
+                next_height_nodes.append(tuple(new_objects))
+
+            elif new_objects[j] == abs(west_east):
+                index.append(h)
+                new_objects[j] = abs(west_east - 1)
+                next_height_nodes.append(tuple(new_objects))
+                new_objects[j] = abs(west_east)
+    count += 1
+    # print("index", index)
+    return next_height_nodes, count, index
+
+def dfs(total_tree, visited, index):
+    stack = [(0, 0)]
+    steps = []
+    while stack:
+        height, index_value = stack.pop()
+        node = total_tree[height][index_value]
+        if node not in visited and is_valid(node):
+            print("Step:", node)
+            steps.append(node)
+            visited.add(node)
+
+            if height + 1 < len(total_tree):
+                for i, child_node in enumerate(total_tree[height + 1]):
+                    if index[height + 1][i] == index_value:  
+                        stack.append((height + 1, i))
+    return steps
+
+
+
+
+
+def main():
+    start_state = (0,0,0,0)
     visited = set()
-    queue = deque([(start_state, [])])
+    tree_structure = []
+    total_tree, index = tree(start_state)
+    steps = dfs(total_tree, visited, index)
+    # print(steps)
 
-    while queue:
-        current_state, actions = queue.popleft()
-        if current_state not in visited:
-            visited.add(current_state)
 
-            if all(item == 1 for item in current_state):
-                return actions
-
-            possible_moves = generate_moves(current_state)
-            for move in possible_moves:
-                if move not in visited and is_valid(move):
-                    queue.append((move, actions + [move]))
-
-    return None
-
-# Depth-First Search algorithm
-def depth_first_search(current_state, visited, actions):
-    visited.add(current_state)
-    if all(item == 1 for item in current_state):
-        return actions
-
-    possible_moves = generate_moves(current_state)
-    for move in possible_moves:
-        if move not in visited and is_valid(move):
-            result = depth_first_search(move, visited, actions + [move])
-            if result:
-                return result
-
-    return None
-
-# Solve using Breadth-First Search
-bfs_solution = breadth_first_search()
-if bfs_solution:
-    print("BFS Solution:")
-    for move in bfs_solution:
-        print(move)
-else:
-    print("BFS Solution not found.")
-
-# Solve using Depth-First Search
-dfs_solution = depth_first_search((0, 0, 0, 0), set(), [])
-if dfs_solution:
-    print("\nDFS Solution:")
-    for move in dfs_solution:
-        print(move)
-else:
-    print("DFS Solution not found.")
+main()
